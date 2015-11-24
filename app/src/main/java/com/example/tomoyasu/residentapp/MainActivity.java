8e4ec68afc18357e3104583c13ddf05126f13163
tomoyasu <tom.kappa0923@gmail.com>
2015-11-24 18:40:56 +0900

    11/24,18:39 ・画面上部にトグルスイッチを追加 ・ボタンをフラットデザイン風に変更

--- app/src/main/java/com/example/tomoyasu/residentapp/MainActivity.java
+++ app/src/main/java/com/example/tomoyasu/residentapp/MainActivity.java
@@ -2,20 +2,49 @@ package com.example.tomoyasu.residentapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends Activity {
+    static final String TAG="LocalService";
+    private Switch sw;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // フォント取得
+        Typeface tf = Typeface.createFromAsset(getAssets(), "mgenplus-2c-regular.ttf");
+
+        // トグルスイッチ
+        sw = (Switch)findViewById(R.id.SwitchButton);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Log.i(TAG, "Switch On");
+                    startService(new Intent(MainActivity.this, NotificationChangeService.class));
+                } else {
+                    Log.i(TAG, "Switch Off");
+                    stopService(new Intent(MainActivity.this, NotificationChangeService.class));
+                }
+            }
+        });
+        sw.setTypeface(tf);
+
+        // 各ボタンの設定
         Button btn = (Button)findViewById(R.id.StartButton);
         btn.setOnClickListener(btnListener);
+        btn.setTypeface(tf);
 
         btn = (Button)findViewById(R.id.StopButton);
         btn.setOnClickListener(btnListener);
+        btn.setTypeface(tf);
     }
 
     private View.OnClickListener btnListener = new View.OnClickListener() {
